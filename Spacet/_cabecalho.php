9d54e951147e0367017a987aaba5544fdbf188e3
HigorBaggio79 <52335755+HigorBaggio79@users.noreply.github.com>
2019-07-04 18:52:32 -0300

    Add files via upload

---
+++ Spacet/_cabecalho.php
@@ -0,0 +1,26 @@
+<?php
+//**************** 
+ //cabeçalho
+//********* */
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spacet</title>
+
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.js">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+</head>
+<body>
+    
+
+
+
---
+++ Spacet/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//=======================================
+//index
+//=======================================
+
+include_once('_cabecalho.php');
+
+
+echo'<p>higor teste do x <span class="fa fa-times"></span></p>';
+
+include_once('_rodape.php');
+
+?>
